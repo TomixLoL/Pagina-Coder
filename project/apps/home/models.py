21e8a2e8dafb17453971da1fa681307ae68ece93
Tomas Greco <grecotomas06@gmail.com>
2023-05-26 15:09:08 -0300

    Update admin de home: se registraron modelos Post y Autor

--- project/apps/home/models.py
+++ project/apps/home/models.py
@@ -4,8 +4,6 @@ from django.db import models
 class Post(models.Model):
     titulo = models.CharField(max_length=100)
     contenido = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.titulo
