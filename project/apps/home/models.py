cd0cfe9ef97cb37693161c68d9beb399da140bd4
Tomas Greco <grecotomas06@gmail.com>
2023-05-26 14:56:26 -0300

    create models: post y autor

--- project/apps/home/models.py
+++ project/apps/home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    titulo = models.CharField(max_length=100)
+    contenido = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.titulo
+
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nombre
