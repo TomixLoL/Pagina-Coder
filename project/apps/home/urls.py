0b4782bc935ab7ff8f5fc2601ba589ff24527fa4
Tomas Greco <grecotomas06@gmail.com>
2023-05-26 16:55:59 -0300

    Update Views: crear_autor

--- project/apps/home/urls.py
+++ project/apps/home/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index" ),
+    path("crear-autor/", views.crear_autor, name="crear_autor"),
+    path("crear-post/", views.crear_post, name="crear_post"),
 ]
--- project/apps/home/views.py
+++ project/apps/home/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . import forms
 
-# Create your views here.
 def index(request):
     return render(request, 'home/index.html')
+
+def crear_autor(request):
+    if request.method == 'POST':
+        form = forms.AutorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home:index')
+    else:
+        form = forms.AutorForm()
+        context = {"form": form}
+        return render(request, 'home/crear_autor.html', context)
+
+def crear_post(request):
+    return render(request, 'home/crear_post.html')
