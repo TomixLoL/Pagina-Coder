af5325f5b1cbcf4d1f5d4fb03796a50e678e81b8
Tomas Greco <grecotomas006@gmail.com>
2023-05-26 22:04:14 -0300

    Implementacion de Template completo

--- project/apps/home/urls.py
+++ project/apps/home/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
 from . import views
 
@@ -7,3 +8,5 @@ urlpatterns = [
     path("crear-post/", views.crear_post, name="crear_post"),
     path("crear-etiqueta/", views.crear_etiqueta, name="crear_etiqueta"),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
